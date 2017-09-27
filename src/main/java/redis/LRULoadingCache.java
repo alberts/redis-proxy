2ba63ad133b5e49b4f09c783c1118d7d614b9172
Beck Chen <csybeck@gmail.com>
2017-09-26 23:35:42 -0700

    Use LinkedHashMap for LRUCache implementation

--- src/main/java/redis/LRULoadingCache.java
+++ src/main/java/redis/LRULoadingCache.java
@@ -10,23 +10,17 @@ import java.util.stream.Collectors;
  */
 public class LRULoadingCache {
 
-    private int maxCapacity;
     private long cacheExpiry;
-    private int currentCapacity;
     private Function<String, String> loadingFunc;
-    private Map<String, String> keyToValue;
+    private LinkedHashMap<String, String> keyToValue;
     private SortedMap<Long, Set<String>> insertionTimeToKeys;
-    private Map<String, Long> keyToLastUsedTime;
     private final ScheduledExecutorService scheduler;
 
     public LRULoadingCache(int capacity, long cacheExpiryInMs, Function<String, String> loadingFunc) {
-        this.maxCapacity = capacity;
-        this.currentCapacity = 0;
         this.cacheExpiry = cacheExpiryInMs;
         this.loadingFunc = loadingFunc;
-        keyToValue = new ConcurrentHashMap<>(capacity);
+        keyToValue = new LRULinkedHashMap<>(capacity, capacity);
         insertionTimeToKeys = new TreeMap<>();
-        keyToLastUsedTime = new HashMap<>();
         scheduler = Executors.newScheduledThreadPool(1);
         scheduler.scheduleAtFixedRate(() -> evictExpiredEntries(), 1,1, TimeUnit.SECONDS);  // evict expired entries every minute
     }
@@ -43,39 +37,15 @@ public class LRULoadingCache {
             String value = loadingFunc.apply(command);
             if (value != null) {  // cache non-null value
                 synchronized (this) {
-                    if (currentCapacity == maxCapacity) {
-                        evictLeastRecentlyUsed();
-                    }
                     keyToValue.put(key, value);
-                    currentCapacity++;
                     insertionTimeToKeys.putIfAbsent(currentTimestamp, new HashSet<>());
                     insertionTimeToKeys.get(currentTimestamp).add(key);
-                    keyToLastUsedTime.put(key, currentTimestamp);
                 }
             }
             cachedValue = value;
-        } else {
-            synchronized (this) {
-                keyToLastUsedTime.put(key, currentTimestamp);
-            }
         }
 
         return cachedValue;
-    }
-
-    private void evictLeastRecentlyUsed() {
-        Long lruTime = Long.MAX_VALUE;
-        String lruKey = null;
-        for (Map.Entry<String, Long> entry : keyToLastUsedTime.entrySet()) {
-            if (entry.getValue() < lruTime) {
-                lruTime = entry.getValue();
-                lruKey = entry.getKey();
-            }
-        }
-        keyToValue.remove(lruKey);
-        keyToLastUsedTime.remove(lruKey);
-        currentCapacity--;
-        System.out.println("Evict " + lruKey + " last used at " + lruTime);
     }
 
     private synchronized void evictExpiredEntries() {
@@ -85,12 +55,29 @@ public class LRULoadingCache {
             for (String key : insertionTimeToKeys.get(ts)) {
                 if (keyToValue.containsKey(key)) {
                     keyToValue.remove(key);
-                    keyToLastUsedTime.remove(key);
-                    currentCapacity--;
                     System.out.println("Evict " + key + " inserted at " + ts);
                 }
             }
             insertionTimeToKeys.remove(ts);
+        }
+    }
+
+    private static class LRULinkedHashMap<K, V> extends LinkedHashMap<K, V> {
+
+        private final int maxEntries;
+
+        LRULinkedHashMap(int initialCapacity, int maxEntries) {
+            super(initialCapacity, 0.75f, true);
+            this.maxEntries = maxEntries;
+        }
+
+        @Override
+        protected boolean removeEldestEntry(Map.Entry<K,V> eldest) {
+            if (size() > maxEntries) {
+                System.out.println("Evict eldest entry");
+                return true;
+            }
+            return false;
         }
     }
 
